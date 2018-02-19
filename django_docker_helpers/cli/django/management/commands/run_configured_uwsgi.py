c19058fa49d8d6149936effb120ac762264284fd
kornov-rooman <kornov.rooman@gmail.com>
2018-02-19 14:21:54 +0300

    refactored run_configured_uwsgi django cli a bit
    added tests
    fixed a bit README.md

--- django_docker_helpers/cli/django/management/commands/run_configured_uwsgi.py
+++ django_docker_helpers/cli/django/management/commands/run_configured_uwsgi.py
@@ -3,7 +3,7 @@ import typing as t
 
 from django.core.management.base import BaseCommand
 
-from django_docker_helpers.config import RedisParser
+from django_docker_helpers.config import ConfigLoader
 
 
 def write_uwsgi_ini_cfg(fp: t.IO, cfg: dict):
@@ -11,14 +11,20 @@ def write_uwsgi_ini_cfg(fp: t.IO, cfg: dict):
     Writes into IO stream the uwsgi.ini file content (actually it does smth strange, just look below).
 
     uWSGI configs are likely to break INI (YAML, etc) specification (double key definition)
-    so it writes config AS IS even without trying to fix it.
+    so it writes `cfg` object (dict) in "uWSGI Style".
 
-    ::
-        [uwsgi]
-        static-map = /static/=/application/static/
-        static-map = /media/=/application/media/
-        static-map = /usermedia/=/application/usermedia/
-
+    >>> import sys
+    >>> cfg = {
+    ... 'static-map': [
+    ... '/static/=/application/static/',
+    ... '/media/=/application/media/',
+    ... '/usermedia/=/application/usermedia/']
+    ... }
+    >>> write_uwsgi_ini_cfg(sys.stdout, cfg)
+    [uwsgi]
+    static-map = /static/=/application/static/
+    static-map = /media/=/application/media/
+    static-map = /usermedia/=/application/usermedia/
     """
     fp.write(f'[uwsgi]\n')
 
@@ -35,51 +41,44 @@ def write_uwsgi_ini_cfg(fp: t.IO, cfg: dict):
 
 
 class Command(BaseCommand):
-    help = 'Runs uWSGI with an INI config fetched from Redis'
+    help = 'Runs uWSGI with an INI config fetched from ConfigLoader'
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'key',
-            help='The Redis String (data type) key, used for getting the String\'s value (YAML config)'
-        )
-        parser.add_argument(
-            '--yml-key', default='uwsgi', dest='yml_key',
+            '--key', default='uwsgi', dest='key',
             help='The YAML configuration key with which uWSGI config can be accessed (default: uwsgi)'
         )
-
         parser.add_argument(
             '--print', action='store_true', dest='print',
             help='If true the ini file content will be outputted'
         )
-
         parser.add_argument(
             '-f', '--file', default='/tmp/uwsgi.ini', dest='file',
             help='Specifies a file that will be used for config storing'
         )
 
-        # Redis's
-        parser.add_argument('--host', default='localhost', dest='host', help='Redis host (default: localhost)')
-        parser.add_argument('--port', default=6379, dest='port', help='Redis port (default: 6379)')
-        parser.add_argument('--db', default=0, dest='db', help='Redis db (default: 0)')
+    def handle(self, **options):
+        key = options['key']
 
-    def handle(self, key, **options):
-        yml_key = options['yml_key']
-        cfg_file = options['file']
+        configure = ConfigLoader.from_env(suppress_logs=True, silent=True)
+        uwsgi_cfg = configure.get(key)
 
-        parser = RedisParser(endpoint=key, host=options['host'], port=options['port'], db=options['db'])
-        cfg = parser.get(yml_key)
+        if not uwsgi_cfg:
+            self.stderr.write(f'Parsing error: uWSGI config was not found by the key "{key}"')
+            return
 
         if options['print']:
             self.stdout.write('*' * 80 + '\n')
             self.stdout.write('uWSGI CONFIG'.center(80))
             self.stdout.write('*' * 80 + '\n')
 
-            write_uwsgi_ini_cfg(self.stdout, cfg)
+            write_uwsgi_ini_cfg(self.stdout, uwsgi_cfg)
 
             self.stdout.write('*' * 80 + '\n')
             self.stdout.flush()
 
+        cfg_file = options['file']
         with open(cfg_file, 'w') as file:
-            write_uwsgi_ini_cfg(file, cfg)
+            write_uwsgi_ini_cfg(file, uwsgi_cfg)
 
         os.execvp('uwsgi', ('--ini', cfg_file))
