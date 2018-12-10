87fcd51e2dc5ff08de79a63d4874a69ca56c3df7
night-crawler <lilo.panic@gmail.com>
2018-12-10 17:07:00 +0300

    fixed some tests, typos; added travis; misc

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
     ],
     install_requires=['dpath', 'pyaml', 'gunicorn', 'django', 'terminaltables']
